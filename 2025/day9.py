45e0816b841d45f39e1ea16f7a91b2fd0c9511b8
Edvinas Šulcas <Edvinas.Sulcas@adform.com>
2025-12-10 18:29:37 +0200

    2025 day 9: another hard one, only part one so far

---
+++ 2025/day9.py
@@ -0,0 +1,41 @@
+﻿import time
+from collections import defaultdict
+from itertools import combinations
+
+
+from utilities.coordinate import Coordinate, print_map
+
+
+def main():
+    with open("input/day9.txt", "r") as file:
+        file_content = file.read().splitlines()
+
+    coordinates = [Coordinate.from_string(line) for line in file_content]
+
+    rectangles = []
+    for c1, c2 in combinations(coordinates, 2):
+        if c1 != c2:
+            rectangles.append((c1, c2, (abs(c1.x - c2.x) + 1) * (abs(c1.y - c2.y) + 1)))
+
+    rectangles.sort(key=lambda x: x[2], reverse=True)
+    print(rectangles[0][2])
+
+    tiles = defaultdict(lambda: '.')
+    previous_coordinate = coordinates[0]
+    for coordinate in coordinates[1:]:
+        between = previous_coordinate.coordinates_straight_between(coordinate)
+        for coord in between:
+            tiles[coord] = '#'
+        previous_coordinate = coordinate
+
+    between = previous_coordinate.coordinates_straight_between(coordinates[0])
+    for coord in between:
+        tiles[coord] = '#'
+
+    print_map(tiles)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("Executed in: %s seconds." % (time.time() - start_time))
