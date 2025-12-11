31d103dc7cd1eef1c2a63fd7a545c65af0660859
Edvinas Šulcas <Edvinas.Sulcas@adform.com>
2025-12-11 15:11:17 +0200

    2025 day 11: recursive memoization ftw

---
+++ 2025/day10.py
@@ -0,0 +1,85 @@
+﻿import time
+from typing import List, Tuple
+
+
+def parse_machine(line: str) -> Tuple[int, List[List[int]], List[int]]:
+    split = line.split()
+    diagram_string = split[0].strip("[]")
+    diagram = 0
+    for i, char in enumerate(diagram_string):
+        if char == "#":
+            diagram |= (1 << i)
+
+    buttons = []
+    for button in split[1:-1]:
+        stripped = button.strip("()")
+        nums = [int(num) for num in stripped.split(",")]
+        buttons.append(nums)
+
+    joltage_target = [int(num) for num in split[-1].strip("{}").split(',')]
+
+    return diagram, buttons, joltage_target
+
+
+def solve(target: int, buttons: List[List[int]]) -> int:
+    start = 0
+
+    steps = 0
+    states = set()
+    states.add(start)
+    while True:
+        steps += 1
+        new_states = set()
+        for state in states:
+            for button in buttons:
+                new_state = state
+                for bit in button:
+                    new_state ^= (1 << bit)
+                if new_state == target:
+                    return steps
+                new_states.add(new_state)
+        states = new_states
+
+
+def multiply(polynomial: List[int], buttons: List[List[int]]):
+    pass
+
+
+def solve_joltage(target: List[int], buttons: List[List[int]]) -> int:
+    polynomial = tuple([0 for _ in range(len(target))])
+
+    result = multiply(polynomial, buttons)
+
+    return 0
+
+
+def main():
+    with open("input/day10.txt", "r") as file:
+        file_content = file.read().splitlines()
+
+    machines = []
+    for line in file_content:
+        machines.append(parse_machine(line))
+
+    # print(machines)
+
+    count = 0
+
+    i = 0
+    for machine in machines:
+        count += solve(machine[0], machine[1])
+
+    print(count)
+
+    count = 0
+    for machine in machines:
+        count += solve_joltage(machine[2], machine[1])
+        print(i)
+        i += 1
+
+    print(count)
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("Executed in: %s seconds." % (time.time() - start_time))
---
+++ 2025/day11.py
@@ -0,0 +1,52 @@
+﻿import time
+
+
+
+def main():
+    with open("input/day11.txt", "r") as file:
+        file_content = file.read().splitlines()
+
+    cache = {}
+    servers = {}
+    for line in file_content:
+        split = line.split()
+        servers[split[0].strip(":")] = split[1:]
+
+    # print(servers)
+
+    complete_paths = count_paths('you', 'out', servers, cache)
+    print(complete_paths)
+
+    start_to_fft = count_paths('svr', 'fft', servers, cache)
+    start_to_dac = count_paths('svr', 'dac', servers, cache)
+    dac_to_fft = count_paths('dac', 'fft', servers, cache)
+    fft_to_dac = count_paths('fft', 'dac', servers, cache)
+    fft_to_out = count_paths('fft', 'out', servers, cache)
+    dac_to_out = count_paths('dac', 'out', servers, cache)
+
+    paths_through_fft_first = start_to_fft * fft_to_dac * dac_to_out
+    paths_through_dac_first = start_to_dac * dac_to_fft * fft_to_out
+    total_paths = paths_through_fft_first + paths_through_dac_first
+    print(total_paths)
+
+
+def count_paths(start: str, end: str, servers: dict[str, str], cache: dict[tuple[str, str], int]) -> int:
+    if (start, end) in cache:
+        return cache[(start, end)]
+
+    if start == end:
+        return 1
+
+    total_paths = 0
+    if start in servers:
+        for neighbor in servers[start]:
+            total_paths += count_paths(neighbor, end, servers, cache)
+
+    cache[(start, end)] = total_paths
+    return total_paths
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("Executed in: %s seconds." % (time.time() - start_time))
